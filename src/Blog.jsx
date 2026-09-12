ffa7046cc068c99afb678f3e3b2eaa7d2933272f
Michael Kruzil <1523573+mkruzil@users.noreply.github.com>
2026-09-12 03:51:55 +0000

    Module 4 Frontend Changes

---
+++ src/Blog.jsx
@@ -0,0 +1,45 @@
+import { PostList } from './components/PostList.jsx'
+import { CreatePost } from './components/CreatePost.jsx'
+import { PostFilter } from './components/PostFilter.jsx'
+import { PostSorting } from './components/PostSorting.jsx'
+
+import { useState } from 'react'
+
+import { useQuery } from '@tanstack/react-query'
+import { getPosts } from './api/posts.js'
+
+export function Blog() {
+
+  const [author, setAuthor] = useState('')
+  const [sortBy, setSortBy] = useState('createdAt')
+  const [sortOrder, setSortOrder] = useState('descending')
+
+  const postQuery = useQuery({
+    queryKey: ['posts', { author, sortBy, sortOrder }],
+    queryFn: () => getPosts({ author, sortBy, sortOrder }),
+  })
+
+  const posts = postQuery.data ?? []
+
+  return (
+    <div style={{ padding: 8 }}>
+      <CreatePost />
+      <br />
+      <hr />
+      Filter by:
+      <PostFilter 
+        field='author' 
+        value={author} 
+        onChange={(value) => setAuthor(value)}/>
+      <br />
+      <PostSorting
+        fields={['createdAt', 'updatedAt']}
+        value={sortBy}
+        onChange={(value) => setSortBy(value)}
+        orderValue={sortOrder}
+        onOrderChange={(orderValue) => setSortOrder(orderValue)}/>
+      <hr />
+      <PostList posts={posts} />
+    </div>
+  )
+}
